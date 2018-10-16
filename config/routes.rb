9581418c0bd4fc8b9525d1c6e3bac3b47dd615a7
Friedaz <rheganandar@gmail.com>
2018-10-16 14:20:04 +0700

    Firts

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
+  get 'new' => 'posts#new'
+
+  resources :pages
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
