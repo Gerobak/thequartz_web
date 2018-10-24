bfbfe1bdaf79b7161c36f232ea31b5427effaaee
Rheganandar <43843938+Friedaz@users.noreply.github.com>
2018-10-24 09:03:29 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :galleries
-
-
-=======
-  resources :events
->>>>>>> fa711ae48ea17591aa8410fd49d3c0b3e0d3224b
+  
   devise_for :users, :controllers => { :sessions => "sessions" }
   root 'posts#index', as: 'home'
   get 'about' => 'pages#about', as: 'about'
   get 'new' => 'posts#new'
 
-
+  resources :galleries
+  resources :events
   resources :pages
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
